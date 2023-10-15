022f34bc4dffbb77a1cc52f18da62031353364d5
ShineCorine <imsjm81@gmail.com>
2023-10-15 16:12:31 +0900

    특정 에러메시지를 지정해서 캐치

--- exception/src/main/java/com/example/exception/exeption/RestApiExceptionHandler.java
+++ exception/src/main/java/com/example/exception/exeption/RestApiExceptionHandler.java
@@ -20,4 +20,13 @@ public class RestApiExceptionHandler {
 
         return ResponseEntity.status(200).build();
     }
+
+    // 원하는 예외만 캐치하기
+    @ExceptionHandler(value = {IndexOutOfBoundsException.class})
+    public ResponseEntity outOfBound(
+            IndexOutOfBoundsException e
+    ){
+        log.error("IndexOutOfBoundsException", e);
+        return ResponseEntity.status(200).build();
+    }
 }
