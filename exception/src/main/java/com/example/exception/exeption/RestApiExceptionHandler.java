3ed239acc9af27472e6a2e45f63fb0c255d103d0
ShineCorine <imsjm81@gmail.com>
2023-10-15 17:07:25 +0900

    Exception handler 에서 에러 처리 코드 추가
    response body에 원하는 코드가 나오도록 핸들러 추가
    그러나 여전히 error임에도 불구하고 200 으로 출력된다 (바디 안의 응답은 404 정상)
    객체를 전달하면 기본적으로 200으로 응답한다는 것과 관련 있는듯

--- exception/src/main/java/com/example/exception/exeption/RestApiExceptionHandler.java
+++ exception/src/main/java/com/example/exception/exeption/RestApiExceptionHandler.java
@@ -3,10 +3,14 @@ package com.example.exception.exeption;
 
 import com.example.exception.controller.RestApiBController;
 import com.example.exception.controller.RestApiController;
+import com.example.exception.model.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
 
 
 @Slf4j
@@ -31,5 +35,27 @@ public class RestApiExceptionHandler {
     ){
         log.error("IndexOutOfBoundsException", e);
         return ResponseEntity.status(200).build();
+    }
+
+    @ExceptionHandler(value = {NoSuchElementException.class})
+    public Api noSuchElement(
+            NoSuchElementException e
+    ){
+        log.error("", e);
+
+        return Api.builder()
+                .resultCode(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                .resultMessage(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .build()
+                ;
+
+        /* 예상 결과값
+        {
+            "result_code": "404",
+            "result_message": "Not Found",
+            "data": null
+}
+        }
+         */
     }
 }
