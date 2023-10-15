30aabb9858d67f7a0f18c6cb1ce9735812d8f30f
ShineCorine <imsjm81@gmail.com>
2023-10-15 19:58:54 +0900

    어노테이션을 활용한 유효성 검증

---
+++ validation/src/main/java/com/example/validation/controller/UserApiController.java
@@ -0,0 +1,35 @@
+package com.example.validation.controller;
+
+
+import com.example.validation.model.UserRegisterRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController {
+
+    @PostMapping("")
+    public UserRegisterRequest register(
+
+        @Valid
+        @RequestBody
+        UserRegisterRequest userRegisterRequest
+    ){
+        log.info("init : {}", userRegisterRequest);
+
+        return userRegisterRequest;
+    }
+}
+
+
+// Resolved [org.springframework.web.bind.MethodArgumentNotValidException:
+// Validation failed for argument [0] in public com.example.validation.model.UserRegisterRequest
+// com.example.validation.controller.UserApiController.register(com.example.validation.model.UserRegisterRequest):
+// [Field error in object 'userRegisterRequest' on field 'registerAt': rejected value [2023-01-29T13:05:10]; codes [FutureOrPresent.userRegisterRequest.registerAt,FutureOrPresent.registerAt,FutureOrPresent.java.time.LocalDateTime,FutureOrPresent]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [userRegisterRequest.registerAt,registerAt]; arguments []; default message [registerAt]]; default message [현재 또는 미래의 날짜여야 합니다]] ]
